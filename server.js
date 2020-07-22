ef37f94265f5ecf3ad0459ace95427c742f8dac5
numeroflip <berenyi.aron@hotmail.hu>
2020-07-22 22:19:10 +0200

    Making it all function quite well. Making the zip to city suggestions dropdown while fetching the API

--- server.js
+++ server.js
@@ -1,71 +1,65 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = 8000;
 const fs = require('fs');
-const classes = require('./server_modules/classes.js')
+// BaseUrl
+
+
+// GET /locations/v1/postalcodes/search?apikey=WJDCsvHiNT5AMwGVS9IvROBmTOYJUjnM&q=9400 HTTP/1.1
+
+
 
 
 
 // Create endpoint
-let appData, appDataChopped;
+let projectData;
 readFile();
 
-function addToData(data) {appData.push(data);};
-function writeFile(file = './data.json') {fs.writeFileSync(file, JSON.stringify(appData) ,'utf8')};    
-function readFile(file = './data.json') {appData = JSON.parse(fs.readFileSync(file, 'utf8'))};
-function chopData(len = 5, dataset = appData) {
-    let choppedData = [[]];
-    dataset.map((data, i) => {
-        console.log(data.length - 1);
-        if (i % len == 0) {choppedData.push([])}
-        choppedData[data.length - 1].push(data)
-        
-    });
-    console.log('THIS IS CHOPPEDDATA')
-    console.log(choppedData[0])
-}
+function addToData(data) {
+    projectData.entries.push(data);
+};
 
-chopData();
+function writeFile(file = './data.json') {
+    fs.writeFileSync(file, JSON.stringify(projectData) ,'utf8')
+};   
+
+function readFile(file = './data.json') {
+    projectData = JSON.parse(fs.readFileSync(file, 'utf8'))
+};
 
 if (!fs.readFileSync('./data.json', 'utf8')) {
-    appData = [];
+    projectData = {entries: [], pages: 1};
     writeFile();
 };
 
-
-
+// MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const cors = require('cors');
 app.use(cors());
 
-// BaseUrl
-// const baseUrl =
-// API Key
-const key = 'WJDCsvHiNT5AMwGVS9IvROBmTOYJUjnM';
+
 // Initalize the server files
 app.use(express.static('website'));
 // Start the server
 app.listen(port, () => console.log(`server is running on port ${port}`));
 // GET route
-app.get('/all', (req, res) => {
-    res.send(appData);
+app.get('/get-data', (req, res) => {
+    console.log('GET requested')
+    res.send(JSON.stringify(projectData));
 });
 console.log('GET route is set');
 // POST route
-app.post('/addEntry', addAndUpdateData);
+app.post('/addEntry', handlePOST);
 console.log('POST route is set');
 
 
-
-function addAndUpdateData(req, res) {
+async function handlePOST(req, res) {
     console.log('POST happened');
+    let postedData = req.body;    
     addToData(req.body);
     writeFile();
     readFile();
-    res.send(appData);
-    console.log(appData);
-
 }
+
--- website/js/app.js
+++ website/js/app.js
@@ -1,42 +1,166 @@
-
-
+//  Declare variables
 const nameField = document.getElementById('name');
-const locationField = document.getElementById('location');
-const textareaField = document.getElementById('textarea-main');
+const locationField = document.getElementById('zip');
+const textareaField = document.getElementById('feelings');
 const titleField = document.getElementById('title-input');
-const submitBtn = document.getElementById('submit-btn');
-const entriesWrapper = document.getElementById('entries-wrapper');
+const submitBtn = document.getElementById('generate');
+const entriesHeader = document.getElementById('entries-header');
+let pageData, possibleLocations;
+const corsPrepend = 'https://cors-anywhere.herokuapp.com/';
+const apiData = {
+  baseUrls: {
+      zip: 'http://dataservice.accuweather.com/locations/v1/postalcodes/search',
+      location: 'http://dataservice.accuweather.com/locations/v1/cities/search',
+      currentWeather: 'http://dataservice.accuweather.com/currentconditions/v1/'
+  },
+  key: 'WJDCsvHiNT5AMwGVS9IvROBmTOYJUjnM'
+};
+
+// Dropdown
+const elem = document.querySelector('.autocomplete');
+const instance = M.Autocomplete.init(elem);
 
 class Entry {
-  constructor({author = "Unknown", title, body }) {
+  constructor({author = "Unknown", title, body, location = {zip: undefined, city: undefined, country: undefined}, weather = undefined }) {
       this.author = author;
       this.title = title;
       this.body = body;
-      this.date = new Date;
-  }
-}
+      // date
+      this.date = {};
+      this.date.date = new Date;
+      this.date.year = this.date.date.getFullYear();
+      this.date.month = this.formatDate(this.date.date.getMonth());
+      this.date.day = this.formatDate(this.date.date.getDay());
+      // location
+      this.location = location;
+      this.weather = weather;
+  };
 
-const getEntryHTML = ({author, title, date, body}) => {
+  formatDate(num)  {
+    if (num < 10) {
+      num = '0' + num;
+    }
+    return num;
+  };
+};
+
+
+async function getLocations(zip) {
+  console.log('location requested');
+  const url = corsPrepend + apiData.baseUrls.zip + '?apikey=' + apiData.key + '&q=' + zip
+  const response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+  return data;
+};
+
+async function getWeather(locationKey) {
+  console.log('getWeatherFired');
+  const url = corsPrepend + apiData.baseUrls.currentWeather + locationKey + '?apikey=' + apiData.key
+  let response = await fetch(url).then((resolve) => resolve.json());
+  response = response[0];
+  const weatherData = {
+    description: response.WeatherText,
+    icon: response.WeatherIcon,
+    iconUrl: `../assets/weather_icons/${this.icon}.png`,
+    tempCels: response.Temperature.Metric.Value
+  }
+  console.log(weatherData);
+  return weatherData;
+};
+// getWeather("497505_PC");
+
+function processLocations(locObjects) {
+  const formattedLocationArray = [];
+
+  locObjects.forEach((locObj) => {
+    const formattedLocation = {
+      zip: locObj.PrimaryPostalCode,
+      city: locObj.EnglishName,
+      country: locObj.Country.EnglishName,
+      locationKey: locObj.Key
+    }
+    formattedLocationArray.push(formattedLocation);
+  });
+  return formattedLocationArray;
+};
+
+function addToDropDown(locations) {
+  let data = {}
+  locations.forEach((location) => {
+    const key = `${location.zip} ${location.city} ${location.country}`;
+    data[key] = null 
+  })
+  instance.updateData(data)
+};
+
+
+async function handleLocationDropdown(e) {
+  const value = e.target.value;
+  console.log('Location change handling fired')
+  instance.updateData({});
+  if(isNaN(value.slice(0, 4))) {e.target.classList.add('invalid')}
+  else {e.target.classList.remove('invalid');};
+
+  if(value.length >= 4 && !isNaN(value)) {
+    let locations = await getLocations(value);
+    possibleLocations = processLocations(locations);
+    addToDropDown(possibleLocations);
+    instance.open();
+  }
+};
+
+
+
+
+
+const getEntryHTML = ({author, date, body, location, weather}) => {
+  formatedDate = `${date.year}-${date.month}-${date.day}`
   const entryHTML = `
-      <article class="card-panel row">
+      <article class=" card-panel row">
           <figure class="col s2 figure">
-              <img src="#" alt="weather-image">
+              <img src="${weather.iconUrl}" alt="${weather.description}">
           </figure>
           <div class="separator col s9">
-              <h3 class="article-title">${title}</h3>
+              <h3 class="article-title">${formatedDate}</h3>
               <p class="article-author">${author}</p>
-              <p class="article-date grey-text text-darken-2">${date.getYear()} - ${date.getMonth()} - ${date.getDay()}</p>
+              <p class="article-date grey-text text-darken-2">
+              ${location.city} ${location.country}
+              </p>
+              <p class="article-body">${body}</p>
           </div>
-          <p class="article-body col s12">${body}</p>
       </article>
   `
+  // ${location.city} - ${location.country}
   return entryHTML;
-}
+};
 
+function insertEntryHTML(entryData) {
+  entriesHeader.insertAdjacentHTML('afterend', getEntryHTML(entryData))
+};
 
+const getPageData = async (url = '/') => {
+  try {
+    console.info('Getting page data...')
+    let data = await fetch (url);
+    data = await data.json();
+
+    return data.entries;
+  } catch(err) {
+    console.log(err)
+  }
+};
+
+function renderPageEntries(entries) {
+  console.log(entries)
+  entries.forEach((entry) => {
+    insertEntryHTML(entry)
+  })
+};
 
 const postData = async ( url = '', data = {}) => {
   console.info('Data is starting to POST...')
+  console.log(data)
   try {
     const response = await fetch(url, {
       method: 'POST', 
@@ -48,9 +172,7 @@ const postData = async ( url = '', data = {}) => {
     });
     const newData = await response.json();
     console.log(newData)
-    // entriesWrapper.insertAdjacentHTML('afterbegin', getEntryHTML(newData))
 
-    
   } catch(error) {
     console.log("error", error);
     console.log("SHIT! SOMETHING IS WRONG WITH POST REQUEST");
@@ -58,49 +180,36 @@ const postData = async ( url = '', data = {}) => {
 };
 
 
-  // const getData = async ( url = '') => {
-  //   console.info('Data is starting to GET...')
-
-  //     try {
-  //       const response = await fetch(url);
-  //       const newData = await response.json();
-  //       console.log(newData);
-  //     } catch(error) {
-  //       console.log("error", error);
-  //       console.log("SHIT! SOMETHING IS WRONG WITH GET REQUEST");
-  //     }
-  // };
-
-  // .then((data) => getData('/all'))
-
-submitBtn.addEventListener('click', handleSubmit)
-
-// TODO: on Submit, update the DOM, adding an entry
-
-function handleSubmit(e) {
-
+async function handleSubmit(e) {
   e.preventDefault();
+  const city = locationField.value.split(" ")[1];
+  console.log(city);
+  const index = possibleLocations.map((location) => location.city).indexOf(city);
+  console.log(index);
 
   const data = {
     title: titleField.value,
     author: nameField.value,
-    location: locationField.value,
+    // zip: locationField.value,
     body: textareaField.value,
+    location: possibleLocations[index]
   }
+  console.log ("submitted and data is defined");
   const entryData = new Entry(data);
-
+  console.log(entryData);
+  entryData.weather = await getWeather(entryData.location.locationKey)
   postData('/addEntry', entryData);
-  entriesWrapper.insertAdjacentHTML('afterbegin', getEntryHTML(entryData))
-  // test
-  console.log('Button submitted');
-
+  // getPageData('/getData').then((entries) => {if(entries) {renderPageEntries(entries)}});
+  insertEntryHTML(entryData);
 
 };
 
-locationField.addEventListener('change', handleLocationField);
+async function init() {
+  
+  locationField.addEventListener('keyup', handleLocationDropdown);
+  submitBtn.addEventListener('click', handleSubmit);
+  getPageData('/get-data').then((entries) => {if(entries) {renderPageEntries(entries)}});
 
-//  TODO: connect to the API, and get location completition
-async function handleLocationField() {
-  const resultArr = await fetch()
 }
 
+init();
