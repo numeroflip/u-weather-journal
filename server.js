1dd8cbf81897c4f237801a947844638eb0901b73
numeroflip <berenyi.aron@hotmail.hu>
2020-07-23 17:39:38 +0200

    writing the readme and formatting the files a bit

--- server.js
+++ server.js
@@ -5,20 +5,26 @@ const app = express();
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
+}
 const fs = require('fs');
-// BaseUrl
-
-
-// GET /locations/v1/postalcodes/search?apikey=WJDCsvHiNT5AMwGVS9IvROBmTOYJUjnM&q=9400 HTTP/1.1
-
-
-
-
 
 // Create endpoint
 let projectData;
 readFile();
 
+// In case data.json is empty, give default values
+if (!fs.readFileSync('./data.json', 'utf8')) {
+    projectData = {entries: [], pages: 1};
+    writeFile();
+};
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+// HELPER FUNCTIONS------------------
 function addToData(data) {
     projectData.entries.push(data);
 };
@@ -31,37 +37,22 @@ function readFile(file = './data.json') {
     projectData = JSON.parse(fs.readFileSync(file, 'utf8'))
 };
 
-if (!fs.readFileSync('./data.json', 'utf8')) {
-    projectData = {entries: [], pages: 1};
-    writeFile();
-};
-
-// MIDDLEWARE
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-
-
 // Initalize the server files
 app.use(express.static('website'));
 // Start the server
 app.listen(port, () => console.log(`server is running on port ${port}`));
 // GET route
 app.get('/get-data', (req, res) => {
-    console.log('GET requested')
     res.send(JSON.stringify(projectData));
 });
-console.log('GET route is set');
 // POST route
 app.post('/addEntry', handlePOST);
-console.log('POST route is set');
-
 
 async function handlePOST(req, res) {
-    console.log('POST happened');
-    let postedData = req.body;    
-    addToData(req.body);
-    writeFile();
-    readFile();
+    try {
+        addToData(req.body);
+        writeFile();
+        readFile();
+    } catch(err) {console.log(err)}
 }
 
